9e3c8660723245b65b474797ae9e411adf183652
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2024-11-15 17:02:31 +0100

    Add files via upload

---
+++ lab6/idiv.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("idiv.c\n\n");
+    
+    int a = -2147483648;
+    int b = -3;
+    
+    int iloraz = a / b;
+    int reszta = a % b;
+    
+    printf("a = %d\n", a);
+    printf("b = %d\n\b", b);
+    
+    printf("iloraz = %d\n", iloraz);
+    printf("reszta = %d\n", reszta);
+    
+    
+    
+    return 0;
+}
